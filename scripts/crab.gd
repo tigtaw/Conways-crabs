4a5577f683650cfd7283f96a850113a66b9fda7a
tigtaw <47674498+tigtaw@users.noreply.github.com>
2026-09-20 03:21:27 -0400

    Added a demo crab and a background

---
+++ scripts/crab.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+## The amount of frames in one gametick
+const FRAME_STEP = 60
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
