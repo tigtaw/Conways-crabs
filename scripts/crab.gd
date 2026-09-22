5a88afb28f78f24e43fa55efae47e11e0657e044
tigtaw <47674498+tigtaw@users.noreply.github.com>
2026-09-21 21:37:15 -0400

    Added Git plugin for VCS. Fixed the animation reset point.

--- scripts/crab.gd
+++ scripts/crab.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 ## The amount of frames in one gametick
 const FRAME_STEP = 60
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
